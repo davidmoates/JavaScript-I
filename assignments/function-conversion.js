e8c1c008b950dce6ecf693380fcfeb456d81bbb7
David Moates <david.m.moates@gmail.com>
2019-08-08 11:54:56 -0500

    finished arrow functions now working on arrays

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -12,20 +12,26 @@ myFunction();
 //   return param;
 // };
 // anotherFunction("Example");
-let anotherFunction = (param) => return param;
+let anotherFunction = param => param;
 
-anotherFunction("Example");
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1,param2) => param1 + param2;
+
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1,param2) => param1 - param2;
+
+console.log(subtract(1,2));
 
 // Stretch
 
